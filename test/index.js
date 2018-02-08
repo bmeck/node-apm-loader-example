4cd55aac30f3e7d0ab627abf33e559474464b72c
Bradley Farias <bfarias@godaddy.com>
2018-02-08 09:07:46 -0600

    style

--- test/index.js
+++ test/index.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 const child_process = require('child_process');
 const assert = require('assert');
-
 const path = require('path');
-const fixture = path.join(__dirname, 'fixture', 'call_readFile.mjs');
+const fixture = name => path.join(__dirname, 'fixture', name);
 const child = child_process.spawn(process.execPath, [
-  '--experimental-modules', '--loader', path.join(__dirname, '..'), fixture, fixture
+  '--experimental-modules', '--loader', path.join(__dirname, '..'), fixture('call_readFile.mjs'), fixture('call_readFile.mjs')
 ]);
 const stdout = [];
 child.stdout.on('data', _ => stdout.push(_));
